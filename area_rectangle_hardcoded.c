b0a318ad05e26ca66954b491f61ddd0ec43b2b82
parthcodes14 <parth.d.chavan14@gmail.com>
2025-06-21 14:01:33 +0530

    Add files via upload
    A beginner-level C program that demonstrates how to calculate the area of a rectangle using fixed values (no user input).

---
+++ area_rectangle_hardcoded.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    // Writing a program to calculate area of a rectangle using hard coded inputs
+
+    int a = 15;
+    int b = 25;
+
+    int c = a * b;
+    printf("The area of a rectangle with length %d and breadth %d is %d", a, b, c);
+    return 0;
+}
