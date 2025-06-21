2b0e9fc875668757ef4f5d9533d451d1ecbffe24
parthcodes14 <parth.d.chavan14@gmail.com>
2025-06-21 14:11:02 +0530

    Add files via upload
    Calculates the area of a rectangle by taking user input for length and breadth using scanf().

---
+++ area_rectangle_userinput.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a,b;
+
+    printf("Enter the length of rectangle: \n");
+    scanf("%d", &a);
+
+    printf("Enter the breadth of rectangle: \n");
+    scanf("%d", &b);
+
+    int c = a*b;
+    printf("The area of rectangle with length %d and breadth %d is %d", a,b,c);
+    return 0;
+}
